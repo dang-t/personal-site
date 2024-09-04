ff44b2f00a0c698d6cb3b1028154edd928092194
dang <danquodev@gmail.com>
2024-09-04 01:12:42 -0400

    style(fonts & theme): move fonts to layout, adds theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,39 +1,16 @@
 import { AppProps } from "next/app";
 import { SWRConfig } from "swr/_internal";
-import localFont from "next/font/local";
+import AppLayout from "@/layouts/AppLayout";
 import "@/styles/globals.css";
-
-// use custom local fonts
-const textFont = localFont({
-    src: "../../public/fonts/SF-Pro-Text-Medium.otf",
-    variable: "--sf-pro-text-medium"
-})
-
-const displayFont = localFont({
-    src: [
-        {
-            path: "../../public/fonts/SF-Pro-Display-Medium.otf",
-            style: "normal",
-            weight: "400"
-        },
-        {
-            path: "../../public/fonts/SF-Pro-Display-Heavy.otf",
-            style: "bold",
-            weight: "700"
-        }
-    ],
-    variable: "--sf-pro-display"
-})
-
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <SWRConfig value={{
             fetcher: (url: string) => fetch(`/api${url}`).then((res) => res.json())
         }}>
-            <main className={`${textFont.variable} ${displayFont.variable} font-text`}>
+            <AppLayout>
                 <Component {...pageProps} />
-            </main>
+            </AppLayout>
         </SWRConfig>
     )
 }
