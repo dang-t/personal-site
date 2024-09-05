74e8cb0cd259d15d0f291a0de5c7ecd1c1d5eb64
dang <danquodev@gmail.com>
2024-09-05 17:03:52 -0400

    feat: add mobile nav and adjustments

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,16 +7,16 @@ export default function Page() {
     return (
         <>
             <section id="cover" className="relative grow mt-[124px] h-[calc(100vh-124px)]">
-                <div id="intro" className="max-w-md">Hello, I'm a computer science student from Virginia learning everything and anything I can about software.</div>
+                <div id="intro" className="max-w-sm sm:max-w-md">Hello, I'm a computer science student from Virginia learning everything and anything I can about software.</div>
                 <motion.div
-                    className="absolute bottom-0 left-0 flex gap-6 w-full mb-[64px]"
+                    className="sm:absolute sm:bottom-0 sm:left-0 flex flex-col sm:flex-row gap-2 sm:gap-6 w-full mt-16 sm:mb-[64px]"
                     transition={{ staggerChildren: 0.15 }}
                     initial="hidden"
                     animate="visible"
                 >
                     {[...Socials, {
                         name: "Resume / CV",
-                        url: "https://drive.google.com/file/d/1ePo9Eg6InDJBIFh3wrRuz_1Cj0ZsI3IL/view"
+                        url: "https://drive.google.com/file/d/1ePo9Eg6InDJBIFh3wrRuz_1Cj0ZsI3IL/view?usp=sharing"
                     }].map((social) => (
                         <motion.span key={social.name} variants={{
                             hidden: { opacity: 0, translateX: -10, filter: "blur(3px)" },
--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,18 +2,28 @@
 import { useEffect, useState } from "react";
 import { PiStarFourFill } from "react-icons/pi";
 import { LuCloudMoon, LuCloudSun } from "react-icons/lu";
+import { motion, AnimatePresence } from "framer-motion";
 import { useTheme } from "next-themes";
-import { motion } from "framer-motion";
 import NextLink from "next/link";
 import Link from "./Link";
 
 export default function Header() {
     const pageSections = ["Projects", "About", "Talks"];
     const [mounted, setMounted] = useState<boolean>(false);
+    const [mobile, setMobile] = useState<boolean>(false);
     const { theme, setTheme } = useTheme();
 
     useEffect(() => {
         setMounted(true);
+
+        if (!mounted) {
+            setMobile(window.innerWidth < 640);
+        }
+
+        // on resize listen for mobile < 640px
+        window.addEventListener("resize", () => {
+            setMobile(window.innerWidth < 640);
+        })
     }, [])
 
     const handleThemeChange = () => {
@@ -35,21 +45,24 @@ export default function Header() {
                 <span className="font-display font-bold text-lg">Dang Tran</span>
             </NextLink>
             <div className="inline-flex items-center gap-6 float-right pt-0.5">
-                <motion.div
-                    className="flex gap-6"
-                    transition={{ staggerChildren: 0.15 }}
-                    initial="hidden"
-                    animate="visible"
-                >
-                    {pageSections.map((section) => (
-                        <motion.span key={section} variants={{
-                            hidden: { opacity: 0, translateX: 15, filter: "blur(3px)" },
-                            visible: { opacity: 1, translateX: 0, filter: "blur(0px)" }
-                        }}>
-                            <Link title={section} href={`/#${section.toLowerCase()}`} />
-                        </motion.span>
-                    ))}
-                </motion.div>
+                <div className="flex gap-6">
+                    <AnimatePresence>
+                        {!mobile && (
+                            <>
+                                {pageSections.map((section) => (
+                                    <motion.span
+                                        key={section}
+                                        initial={{ opacity: 0, translateX: 15, filter: "blur(3px)" }}
+                                        animate={{ opacity: 1, translateX: 0, filter: "blur(0px)" }}
+                                        exit={{ opacity: 0, translateX: 15, filter: "blur(3px)" }}
+                                    >
+                                        <Link title={section} href={`/#${section.toLowerCase()}`} />
+                                    </motion.span>
+                                ))}
+                            </>
+                        )}
+                    </AnimatePresence>
+                </div>
                 {/* theme button */}
                 <div className="w-6 h-6">
                     {!mounted ? null :
@@ -70,6 +83,31 @@ export default function Header() {
             </div>
             <div id="blur-overlay-top" className="absolute top-full left-0 bg-gradient-to-t from-transparent to-[--background] backdrop-blur-sm w-full h-[44px] z-10" />
             <div id="blur-overlay-bottom" className="fixed bottom-0 left-0 bg-gradient-to-t from-[--background] to-transparent backdrop-blur-sm w-full h-[64px] z-10" />
-        </header>
+
+            {/* mobile nav */}
+            <span className="fixed bottom-0 left-1/2 -translate-x-1/2 z-20">
+                <AnimatePresence>
+                    {mobile && (
+                        <motion.nav
+                            className="flex justify-center bg-[--background] mb-10 border-[1px] border-black/20 dark:border-white/20 rounded-full overflow-hidden"
+                            initial={{ opacity: 0, width: 0 }}
+                            animate={{ opacity: 1, width: 280 }}
+                            exit={{ opacity: 0, width: 0 }}
+                        >
+                            {pageSections.map((section) => (
+                                <NextLink
+                                    className="group even:border-x-[1px] border-black/20 dark:border-white/20 px-1 py-1"
+                                    href={`/#${section.toLowerCase()}`}
+                                >
+                                    <div className="group-hover:bg-black/10 dark:group-hover:bg-white/10 duration-100 px-4 py-1 rounded-full">
+                                        <span>{section}</span>
+                                    </div>
+                                </NextLink>
+                            ))}
+                        </motion.nav>
+                    )}
+                </AnimatePresence>
+            </span>
+        </header >
     )
 }
