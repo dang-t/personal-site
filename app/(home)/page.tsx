8c7d081766f1505fa5232c946f13cedbecba3432
dang <danquodev@gmail.com>
2024-09-08 04:50:23 -0400

    chore: revert to base, unsatisfied

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,32 +1,9 @@
 "use client"
-import { Socials } from "@/_data/socials";
-import { motion } from "framer-motion";
-import Link from "@/_components/Link";
 
 export default function Page() {
     return (
-        <>
-            <section id="cover" className="relative grow pt-[124px] h-screen">
-                <div id="intro" className="max-w-sm sm:max-w-md">Hello, I'm a computer science student from Virginia learning everything and anything I can about software.</div>
-                <motion.div
-                    className="sm:absolute sm:bottom-0 sm:left-0 flex flex-col sm:flex-row gap-2 sm:gap-6 w-full mt-16 sm:mb-[64px]"
-                    transition={{ staggerChildren: 0.15 }}
-                    initial="hidden"
-                    animate="visible"
-                >
-                    {[...Socials, {
-                        name: "Resume / CV",
-                        url: "https://drive.google.com/file/d/1ePo9Eg6InDJBIFh3wrRuz_1Cj0ZsI3IL/view?usp=sharing"
-                    }].map((social) => (
-                        <motion.span key={social.name} variants={{
-                            hidden: { opacity: 0, translateX: -10, filter: "blur(3px)" },
-                            visible: { opacity: 1, translateX: 0, filter: "blur(0px)" }
-                        }}>
-                            <Link title={social.name} href={social.url} target="_blank" />
-                        </motion.span>
-                    ))}
-                </motion.div>
-            </section>
-        </>
+        <div>
+            index
+        </div>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { textFont, displayFont } from "./_lib/fonts";
-import Header from "./_components/Header";
 import "@/globals.css";
 
 export const metadata: Metadata = {
@@ -14,11 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <head />
             <body className={`${textFont.variable} ${displayFont.variable} font-text`}>
                 <ThemeProvider attribute="class">
-                    <div id="blur-overlay-top" className="fixed top-0 left-0 bg-gradient-to-t from-transparent to-[--background] backdrop-blur-[1px] w-full h-[44px] mt-[72px] z-10" />
-                    <div id="blur-overlay-bottom" className="fixed bottom-0 left-0 bg-gradient-to-t from-[--background] to-transparent backdrop-blur-[1px] w-full h-[64px] z-10" />
-
-                    <Header />
-                    <main className="px-8 max-w-4xl mx-auto">{children}</main>
+                    <main>{children}</main>
                 </ThemeProvider>
             </body>
         </html >
